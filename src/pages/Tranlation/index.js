37dcd0ed8f9f13754b2b400ae26ef40ebcaa4a15
JP-Barbaresco <jpcsb11@gmail.com>
2023-09-24 12:36:29 -0300

    Adiconando tela de home

---
+++ src/pages/Tranlation/index.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../../components/Header'
+
+export default function Tranlation() {
+    return (
+        <View style={styl}>
+
+        </View>
+    )
+}
